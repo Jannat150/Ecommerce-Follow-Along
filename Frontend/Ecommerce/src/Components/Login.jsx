d41a07030de97cb9cc238330a4d1d42d70179611
Jannat150 <cshsjannat2987@gmail.com>
2025-01-17 15:43:44 +0530

    Added login page

---
+++ Frontend/Ecommerce/src/Components/Login.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div className='w-80 h-60 bg-slate-300 rounded-2xl flex-col justify-center items-center mx-auto my-20 shadow-lg'>
+        <h1 className='text-4xl'>Login</h1>
+      <div >
+        <input className='p-3 w-60 m-3 rounded-2xl  ' type="text" placeholder='Enter your username'/>
+      </div>
+      <div>
+        <input className='p-3 w-60 m-3 rounded-2xl' type='password'placeholder='Enter your password'/>
+      </div>
+      <button className='bg-white p-1 border border-black rounded-2xl hover:shadow-md'>Submit</button>
+    </div>
+  )
+}
+
+export default Login
