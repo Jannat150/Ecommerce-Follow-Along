f9f38a9882f245381be8da077d2f8cb865241c46
Jannat150 <cshsjannat2987@gmail.com>
2025-01-20 13:37:06 +0530

    connected mongoDB

--- Backend/server.js
+++ Backend/server.js
@@ -1,19 +1,66 @@
-const express =require('express');
+const express=require('express')
 const mongoose=require('mongoose')
+const { userModel } = require('./model/user.model.js')
+const multer=require('multer')
 const app=express()
-const PORT=9000 
-const mongoURL="mongodb+srv://Jannat:jannat10175168@jannat.5n3xo.mongodb.net/EcomDB";
-let connection=mongoose.connect(mongoURL)
-app.get("/ping",(req,res)=>{
-    res.send("hi");
+const PORT=8088
+
+app.use(express.json())
+
+let mongoURL="mongodb+srv://Jannat:jannat10175168@jannat.5n3xo.mongodb.net/EcomDB"
+let connection=mongoose.connect(mongoURL);
+
+app.get('/ping',(req,res)=>{
+    console.log(req)
+    try {
+        res.send("Pong")
+    } catch (error) {
+        console.log(error)
+        res.send(error)
+    }
 })
+
+const storage=multer.diskStorage({
+    destination:function(req,file,cb){
+        cb(null,'uploads/')
+    },
+    filename:function(req,file,cb){
+        cb(null,file.fieldname+'-'+Date.now()+"-"+file.originalname)
+    }
+});
+
+const upload=multer({storage:storage});
+
+app.post("/upload",upload.single("myFile"),(req,res)=>{
+    try{
+        console.log(req.file)
+        res.send({"message":"file uploaded sucessfully"});
+    }catch(error){
+        console.log(error);
+        res.send({error:"error"})
+    }
+})
+app.post('/create',async(req,res)=>{
+    let payload=req.body
+    console.log(payload)
+    try{
+        let new_user=new userModel(payload);
+        await new_user.save();
+        res.send({"message":"Hurray! Successfully saved the user to the database"})
+    }
+    catch(error){
+        console.log(error);
+        res.send({"error":error})
+    }
+})
+
 app.listen(PORT,async()=>{
     try{
         await connection;
-        console.log("successfully connected")
+        console.log("Successfully connected to mongoDb")
     }
     catch(error){
         console.log(error)
     }
-    console.log("yey")
+    console.log(`Server is running on port ${PORT}`)
 })
